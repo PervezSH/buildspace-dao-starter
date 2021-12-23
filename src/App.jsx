76655a5b5729604d22cf857a96049426e9293874
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-23 23:47:21 +0530

    feat ( dashboard) : add connect to wallet 🔌

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useMemo, useState } from "react";
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // Connect user's wallet using the connectWallet hook thirdweb gives us if user has connected to our web app
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  // If user hasn't connected their wallet before
+  if (!address) {
+    return(
+      <div className="landing">
+        <h1>
+          Welcome to SpaceDAO
+        </h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // If user has connect their wallet
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
